7a5595214f6443613dd1cff90b81eb031bb17824
xiexuan666 <3025366595@qq.com>
2020-01-17 09:27:37 +0800

    静态页面完成

---
+++ components/header.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="header">
+       <el-row type="flex" justify="space-between" class="main">
+      <!-- logo -->
+      <div class="logo">
+        <nuxt-link to="/">
+        <img src="http://157.122.54.189:9093/images/logo.jpg" alt="">
+        </nuxt-link>
+      </div>
+      <!-- 菜单栏 -->
+      <el-row type="flex" class="navs">
+          <nuxt-link to="/">首页</nuxt-link>
+          <nuxt-link to="/post">旅游攻略</nuxt-link>
+           <nuxt-link to="/hotel">酒店</nuxt-link>
+            <nuxt-link to="/air">国内机票</nuxt-link>
+      </el-row>
+      <!-- 登录用户信息 -->
+      <el-row type="flex" align="middle"> 
+          <nuxt-link to="/user/logo" class="account-link" >登录/注册</nuxt-link>  
+      </el-row>
+       </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scopend lang='less'>
+.header{
+    height: 60px;
+    line-height: 60px;
+    background: #fff;
+    border-bottom: 1px solid #ddd;
+
+}
+.main{
+    width: 1000px;
+}
+.logo{
+    width: 156px;
+    padding-top: 8px;
+    img{
+       display: block;
+       width: 100%;
+    }
+}
+.navs{
+    margin: 0 20px;
+    flex: 1;
+
+    a{
+        display: block;
+        padding: 0 20px;
+        height: 60px;
+        box-sizing: border-box;
+
+        &:hover,:focus,&active {
+            border-bottom: 5px solid #409eff;
+            color: #409eff;
+        }
+    }
+    /deep/.nuxt-link-exact-active {
+        background: #409eff;
+        color: #fff!important;
+    }
+}
+.message{
+    height: 36px;
+    line-height: 1;
+    cursor: pointer;
+    .el-icon-bell{
+        margin-right: 2px;
+        font-size: 18px;
+    }
+}
+.el-dropdown-link{
+    margin-left: 20px;
+
+    &.hover {
+        img {
+            border-color: #409eff;
+        }
+    }
+    a{
+        display: block;
+    }
+    img {
+        width: 32px;
+        height: 32px;
+        vertical-align:middle;
+        border: 2px #fff solid;
+        border-radius: 50px;
+    }
+}
+.account-link{
+    font-size: 14px;
+    margin-left: 10px;
+    color: #666;
+    &:hover{
+        color: #409eff;
+        text-decoration: underline;
+    }
+}
+
+</style>
---
+++ pages/user/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login">
+用户登录注册页面
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
